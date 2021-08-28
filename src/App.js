7af16927124146232a4a577c9adfc3c428ec8c13
Elliot Blanchard <git@elliotblanchard.com>
2021-08-28 16:55:33 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { useRef, useState } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { MapControls, OrthographicCamera } from '@react-three/drei';
+
+function Box(props) {
+  // This reference will give us direct access to the mesh
+  const ref = useRef()
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+  // Rotate mesh every frame, this is outside of React without overhead
+  // useFrame(() => {
+    // ref.current.rotation.x = ref.current.rotation.y += 0.01
+  // })
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={active ? 1.5 : 1}
+      onClick={(e) => setActive(!active)}
+      onPointerOver={(e) => setHover(true)}
+      onPointerOut={(e) => setHover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
+
+export default function App() {
+  return (
+    <Canvas>
+      <OrthographicCamera makeDefault position={[0, 0, 10]} zoom={40} />
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <pointLight position={[-10, -10, -10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+      <MapControls />
+    </Canvas>
+  )
+}
