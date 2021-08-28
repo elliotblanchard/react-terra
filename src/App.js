66090a1c06b4a4608174f0c8e99239005626c4d7
Elliot Blanchard <git@elliotblanchard.com>
2021-08-28 18:18:58 -0400

    Added line of blocks - next to-do is get Orthographic cam pointed correctly

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useRef, useState } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { MapControls, OrthographicCamera } from '@react-three/drei';
+import { OrbitControls, MapControls, OrthographicCamera } from '@react-three/drei';
 
 function Box(props) {
   // This reference will give us direct access to the mesh
   const ref = useRef()
+  console.log(props.size)
   // Set up state for the hovered and active state
   const [hovered, setHover] = useState(false)
   const [active, setActive] = useState(false)
@@ -20,22 +21,26 @@ function Box(props) {
       onClick={(e) => setActive(!active)}
       onPointerOver={(e) => setHover(true)}
       onPointerOut={(e) => setHover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
+      castShadow receiveShadow  
+      <boxGeometry args={props.size} />
       <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
     </mesh>
   )
 }
 
 export default function App() {
+  const mapElements = [1.0,2.0,1.0,4.0,3.0,1.0,2.5,4.5,1.5,1.0];
   return (
-    <Canvas>
+    <Canvas shadows>
       <OrthographicCamera makeDefault position={[0, 0, 10]} zoom={40} />
       <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <spotLight castShadow position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+      {mapElements.map((mapUnit,index) =>
+            <Box position={[index, mapUnit/2, 0]} size={[1,mapUnit,1]}/>
+        )}      
       <MapControls />
+      {/* <OrbitControls /> */}
     </Canvas>
   )
 }
