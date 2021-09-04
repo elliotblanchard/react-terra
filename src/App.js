2c1b4b345ddeeb5e00f367c7ee0c766e94b82ee1
elliotblanchard <github@elliotblanchard.com>
2021-09-04 12:07:30 -0400

    Implemented camera lookAt and MapControls

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useRef, useState } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls, MapControls, OrthographicCamera } from '@react-three/drei';
+import * as THREE from 'three';
+
+const lookAtCubePosition = new THREE.Vector3()
 
 function Box(props) {
   // This reference will give us direct access to the mesh
@@ -28,18 +31,51 @@ function Box(props) {
   )
 }
 
+function CameraTarget() {
+  const ref = useRef()
+  // useFrame((state, delta) => {
+  //   if (ref.current.position.x > 3) {
+  //     ref.current.position.x = -3
+  //   } else {
+  //     ref.current.position.x += delta * 1
+  //   }
+  // })
+
+  useFrame((state) => {
+    lookAtCubePosition.x = ref.current.position.x
+    lookAtCubePosition.y = ref.current.position.y
+    lookAtCubePosition.z = ref.current.position.z
+    // lookAtCubePosition.x = 0
+    // lookAtCubePosition.y = 0
+    // lookAtCubePosition.z = 0    
+    state.camera.lookAt(lookAtCubePosition)
+  })
+
+  return (
+    <mesh position={[0, 0.5, 0]} ref={ref}>
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshNormalMaterial attach="material" />
+    </mesh>
+  )
+}
+
 export default function App() {
   const mapElements = [1.0,2.0,1.0,4.0,3.0,1.0,2.5,4.5,1.5,1.0];
+  // const lookAtCubePosition = new THREE.Vector3();
+
   return (
-    <Canvas shadows>
-      <OrthographicCamera makeDefault position={[0, 0, 10]} zoom={40} />
+    <Canvas orthographic camera={{ near: 0.1, far: 1000, position: [3, 3, 3], zoom: 20 }}>
+      {/* <OrthographicCamera makeDefault position={[0, 0, 10]} zoom={40} /> */}
       <ambientLight intensity={0.5} />
       <spotLight castShadow position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
+      {/* <pointLight position={[-10, -10, -10]} /> */}
+      <CameraTarget />
       {mapElements.map((mapUnit,index) =>
             <Box position={[index, mapUnit/2, 0]} size={[1,mapUnit,1]}/>
-        )}      
-      <MapControls />
+        )}   
+        {/* <Box position={[0, 0, 0]} size={[1,1,1]}/>            */}
+      <gridHelper />
+      <MapControls />      
       {/* <OrbitControls /> */}
     </Canvas>
   )
