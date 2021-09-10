447ad7dfa3647a20414a826f9a179763d973df14
elliotblanchard <github@elliotblanchard.com>
2021-09-09 21:43:20 -0400

    Implemented randomize grid button

--- src/App.js
+++ src/App.js
@@ -64,6 +64,8 @@ function CameraTarget() {
 
 export default function App() {
 
+  const [mapElements, setMapElements] = useState(Array(10).fill(Array(10).fill(1)))
+
   const initMap = (dimension) => {
     const getRandomInt = (max) => {
       return Math.floor(Math.random() * max);
@@ -79,54 +81,57 @@ export default function App() {
     return map;
   }
 
-  const mapElements = initMap(10);
+  // setMapElements(initMap(10));
   // const lookAtCubePosition = new THREE.Vector3();
 
   return (
-    <Canvas 
-      orthographic
-      shadows
-      // gl={{
-      //   stencil: false,
-      //   depth: false,
-      //   alpha: false,
-      //   antialias: false,
-      // }}       
-      camera={{ 
-        near: 0.01, 
-        far: 1000, 
-        position: [3, 3, 3], 
-        zoom: 20, 
-      }}>
-      {/* <OrthographicCamera makeDefault position={[0, 0, 10]} zoom={40} /> */}
-      <color attach="background" args={["#eee"]} />      
-      <ambientLight intensity={0.5} />
-      <directionalLight
-        position={[50, 50, 25]}
-        angle={0.3}
-        intensity={2}
-        castShadow
-        shadow-mapSize-width={64}
-        shadow-mapSize-height={64}
-        shadow-camera-left={-10}
-        shadow-camera-right={10}
-        shadow-camera-top={10}
-        shadow-camera-bottom={-10}
-      />
-      <directionalLight position={[-10, -10, -5]} intensity={0.5} />      
-      {/* <spotLight castShadow position={[10, 10, 10]} angle={0.15} penumbra={1} /> */}
-      {/* <pointLight position={[-10, -10, -10]} /> */}
-      <CameraTarget />
-      {mapElements.map((row,i) =>
-            row.map((item,j) =>
-              <Box position={[(row.length*-1)+i, item/2, (row.length*-1)+j]} size={[1,item,1]}/>
-            )
-        )}   
-        {/* <Box position={[0, 0, 0]} size={[1,1,1]}/>            */}
-      {/* <gridHelper /> */}
-      <MapControls />   
-      {/* <Post />    */}
-      {/* <OrbitControls /> */}
-    </Canvas>
+    <>
+      <div onClick={() => setMapElements(initMap(10))}>Randomize Grid</div>
+      <Canvas 
+        orthographic
+        shadows
+        // gl={{
+        //   stencil: false,
+        //   depth: false,
+        //   alpha: false,
+        //   antialias: false,
+        // }}       
+        camera={{ 
+          near: 0.01, 
+          far: 1000, 
+          position: [3, 3, 3], 
+          zoom: 20, 
+        }}>
+        {/* <OrthographicCamera makeDefault position={[0, 0, 10]} zoom={40} /> */}
+        <color attach="background" args={["#eee"]} />      
+        <ambientLight intensity={0.5} />
+        <directionalLight
+          position={[50, 50, 25]}
+          angle={0.3}
+          intensity={2}
+          castShadow
+          shadow-mapSize-width={64}
+          shadow-mapSize-height={64}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
+        <directionalLight position={[-10, -10, -5]} intensity={0.5} />      
+        {/* <spotLight castShadow position={[10, 10, 10]} angle={0.15} penumbra={1} /> */}
+        {/* <pointLight position={[-10, -10, -10]} /> */}
+        <CameraTarget />
+        {mapElements.map((row,i) =>
+              row.map((item,j) =>
+                <Box position={[(row.length*-1)+i, item/2, (row.length*-1)+j]} size={[1,item,1]}/>
+              )
+          )}   
+          {/* <Box position={[0, 0, 0]} size={[1,1,1]}/>            */}
+        {/* <gridHelper /> */}
+        <MapControls />   
+        {/* <Post />    */}
+        {/* <OrbitControls /> */}
+      </Canvas>
+    </>
   )
 }
